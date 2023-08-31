fdf0a2cc955e40b334caff4f695e7d802483781f
Alejandro Casado <alejandroacasadoc@gmail.com>
2023-08-31 13:41:08 -0400

    Task 13

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,10 +3,10 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
+const axios = require('axios');
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
   const { username, password } = req.body;
 
   // Check if username or password is missing
@@ -33,32 +33,33 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Muestra los libros
-  res.send(JSON.stringify(books));
-});
+public_users.get('/', async function (req, res) {
+    res.send(await JSON.stringify(books));
+  });
+
+
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get('/isbn/:isbn',async function (req, res) {
   //libros filtrados por isbn
   const isbn = req.params.isbn;
-  return res.send(books[isbn]);
+  return res.send(await books[isbn]);
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+public_users.get('/author/:author',async function (req, res) {
   //filtrar por nombre de autor
   const author = req.params.author;
-  const filteredBooks = Object.values(books).filter(book => book.author === author);
+  const filteredBooks = await Object.values(books).filter(book => book.author === author);
   // Return the filtered books
   return res.send(filteredBooks);
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+public_users.get('/title/:title',async function (req, res) {
   //filtrar por titulo
   const title = req.params.title;
-  const filteredBooks = Object.values(books).filter(book => book.title === title);
+  const filteredBooks = await Object.values(books).filter(book => book.title === title);
   // Return the filtered books
   return res.send(filteredBooks);
 
