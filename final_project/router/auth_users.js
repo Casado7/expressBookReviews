a46458523109ae2041351d2604e32e7061543e84
Alejandro Casado <alejandroacasadoc@gmail.com>
2023-08-31 12:32:46 -0400

    Task 9

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -55,9 +55,68 @@ regd_users.post("/login", (req,res) => {
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+    const isbn = req.params.isbn;
+    const reviewText = req.query.review; // Get review text from query parameter
+    const username = req.session.authorization.username; // Get username from session
+  
+    if (!isbn || !reviewText) {
+      return res.status(400).json({ message: "ISBN and review text are required." });
+    }
+  
+    // Find the user in the users array
+    const userIndex = users.findIndex(user => user.username === username);
+    if (userIndex !== -1) {
+      // Find the book in the books object
+      const book = books[isbn];
+      
+      if (book) {
+        // Find existing review by the same user for the same ISBN
+        const existingReview = book.reviews[username];
+  
+        if (existingReview) {
+          // Update existing review
+          book.reviews[username] = reviewText;
+        } else {
+          // Add new review for the user
+          book.reviews[username] = reviewText;
+        }
+  
+        return res.status(200).json({ message: "Review added/modified successfully." });
+      } else {
+        return res.status(404).json({ message: "Book not found." });
+      }
+    } else {
+      return res.status(401).json({ message: "User not authorized." });
+    }
+
+  });
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const isbn = req.params.isbn;
+    const username = req.session.authorization.username; // Get username from session
+  
+    // Find the user in the users array
+    const userIndex = users.findIndex(user => user.username === username);
+    if (userIndex !== -1) {
+      // Find the book in the books object
+      const book = books[isbn];
+  
+      if (book) {
+        // Check if the user has an existing review for the book
+        if (book.reviews[username]) {
+          // Delete the user's review
+          delete book.reviews[username];
+          return res.status(200).json({ message: "Review deleted successfully." });
+        } else {
+          return res.status(404).json({ message: "Review not found for this user and book." });
+        }
+      } else {
+        return res.status(404).json({ message: "Book not found." });
+      }
+    } else {
+      return res.status(401).json({ message: "User not authorized." });
+    }
+  });
+  
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
